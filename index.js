036bf8dd83ba5722ee0c0149b9622e4f05bcb14e
masterdude <xmertsahin@gmail.com>
2023-02-13 15:08:08 +0300

    Added rainbow mode

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ let gridInput = document.querySelector(".grid-setter").value;
 
 const eraser = document.querySelector(".eraser");
 
+const rainbow = document.querySelector(".rainbow");
+
 const reset = document.querySelector(".reset");
 
 let color = "black";
 
 let isEraserActive = false;
 
+let isRainbowActive = false;
 
 let gridNumber = gridInput * gridInput;
 
@@ -24,96 +27,161 @@ let drag = false;
 
 
 createGrid(6, 36);
-changeColor();
+paintPixel();
+activateEraser();
+setGrid();
+inputColorChanger();
+resetGrid();
+rainbowMode();
 
 
-function changeColor() {
-   myDivs= document.querySelectorAll(".sketch");
-   for (let index = 0; index < myDivs.length; index++) {
-    myDivs[index].addEventListener("mousedown", function(){
-        console.log(this);
-        drag = true;
-        this.style.backgroundColor = color;
+
+function paintPixel() {
+
+    myDivs = document.querySelectorAll(".sketch");
+
+    for (let index = 0; index < myDivs.length; index++) {
+        myDivs[index].addEventListener("mousedown", function () {
+
+            drag = true;
+            this.style.backgroundColor = colorGen();
+        })
+
+        myDivs[index].addEventListener("mouseover", function () {
+
+
+
+            switch (drag) {
+                case true:
+                    this.style.backgroundColor = colorGen();
+                    break;
+
+                default:
+                    "This is an easter egg!"
+                    break;
+            }
+
+            myDivs[index].addEventListener("mouseup", function () {
+
+                drag = false;
+
+            })
+        })
+
+    }
+}
+
+function setGrid() {
+    gridSetter.addEventListener("input", function (event) {
+
+        document.querySelector(".container").innerHTML = '';
+        gridInput = document.querySelector("input").value;
+
+        gridNumber = gridInput * gridInput;
+
+        createGrid(gridInput, gridNumber);
+        paintPixel();
+
+    });
+}
+
+
+function inputColorChanger() {
+    colorSetter.addEventListener("input", function (event) {
+        color = this.value;
     })
-    myDivs[index].addEventListener("mouseover", function(){
-        switch (drag && !isEraserActive) {
+}
+
+
+
+function resetGrid() {
+    reset.addEventListener("click", function () {
+        let divsToReset = document.querySelectorAll(".sketch");
+        for (let index = 0; index < divsToReset.length; index++) {
+            divsToReset[index].style.backgroundColor = "white";
+
+        }
+    })
+}
+
+
+function activateEraser() {
+    eraser.addEventListener("click", function (event) {
+        isEraserActive = !isEraserActive;
+        console.log(isEraserActive);
+        switch (isEraserActive ) {
             case true:
-                this.style.backgroundColor = color;
+                eraser.classList.add("eraser-activated");
+                toggleColorPicker();
+                rainbow.disabled = true;
                 break;
-        
+
+            case false:
+                eraser.classList.remove("eraser-activated");
+                toggleColorPicker();
+                break;
+
             default:
-                "This is an easter egg!"
                 break;
         }
-        myDivs[index].addEventListener("mouseup", function(){
-            console.log(this);
-            drag = false;
-            
-        })
     })
-    
 
-    
-}
 }
 
-
-gridSetter.addEventListener("input", function(event){
-
-    document.querySelector(".container").innerHTML = '';
-    gridInput = document.querySelector("input").value;
-
-    gridNumber = gridInput * gridInput;
-    console.log("event");
-    
-    createGrid(gridInput, gridNumber);
-    changeColor();
-    
-});
-
-colorSetter.addEventListener("input", function(event) {
-    color = this.value;
-})
-
-reset.addEventListener("click", function () {
-    let divsToReset = document.querySelectorAll(".sketch");
-    for (let index = 0; index < divsToReset.length; index++) {
-        divsToReset[index].style.backgroundColor = "white";
-        
-    }
-})
-
-eraser.addEventListener("click", function(event){
-    isEraserActive = !isEraserActive;
-    console.log(isEraserActive);
-    switch (isEraserActive) {
-        case true:
-            eraser.classList.add("eraser-activated");
-            color = "white";
-            break;
-
-        case false:
-            eraser.classList.remove("eraser-activated");
-            color = "black";
-            break;    
-    
-        default:
-            break;
-    }
-})
 
 
 function createGrid(a, b) {
-    
+
     document.querySelector(".container").style.gridTemplateColumns = `repeat(${a} , auto)`;
     document.querySelector(".container").style.gridTemplateRows = `repeat(${a} , auto)`;
+
     for (let index = 0; index < b; index++) {
-    
+
         let node = document.createElement("div");
         node.classList.add("sketch");
         document.querySelector(".container").appendChild(node);
-        
+
     }
 }
 
 
+function toggleColorPicker() {
+
+    colorSetter.disabled = !colorSetter.disabled;
+}
+
+
+function rainbowMode() {
+
+    rainbow.addEventListener("click", function () {
+        isRainbowActive = !isRainbowActive;
+        switch (isRainbowActive) {
+            case true:
+                rainbow.classList.add("rainbow-activated");
+                break;
+
+            case false:
+                rainbow.classList.remove("rainbow-activated");
+                break;
+        }
+
+    })
+}
+
+
+function colorGen() {
+    let colorArray = [];
+    for (let index = 0; index < 3; index++) {
+        let rgb = Math.floor(Math.random() * 256);
+        colorArray.push(rgb);
+
+    }
+
+    if (isEraserActive) {
+        return "white";
+    } else if (isRainbowActive) {
+        return "rgb(" + colorArray[0] + "," + colorArray[1] + "," + colorArray[2] + ")"
+    } else {
+        return color;
+    }
+}
